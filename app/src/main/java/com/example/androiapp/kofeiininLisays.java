382576e795bbfb44cd04c32ef59d7193a5d51564
harva <harvala.perttu@gmail.com>
2022-04-26 14:51:13 +0300

    aktivityt

---
+++ app/src/main/java/com/example/androiapp/kofeiininLisays.java
@@ -0,0 +1,14 @@
+package com.example.androiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kofeiininLisays extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kofeiinin_lisays);
+    }
+}
---
+++ app/src/main/java/com/example/androiapp/tiedot.java
@@ -0,0 +1,14 @@
+package com.example.androiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tiedot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tiedot);
+    }
+}
