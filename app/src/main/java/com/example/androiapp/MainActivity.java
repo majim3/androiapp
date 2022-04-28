a169dbc3a8b85187d0612188db7cc119e4d4076a
majim3 <45204846+majim3@users.noreply.github.com>
2022-04-28 19:42:39 +0300

    alku

--- app/src/main/java/com/example/androiapp/MainActivity.java
+++ app/src/main/java/com/example/androiapp/MainActivity.java
@@ -3,26 +3,35 @@ package com.example.androiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText fakta;
-    private Button faktaPainike;
+    private TextView fakta;
+    private int rand;
+    private String[] faktoja;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView teksti = (TextView) findViewById(R.id.saldo);
-        fakta = (EditText) findViewById(R.id.faktaText) ;
-    }
+        fakta = (TextView) findViewById(R.id.faktaText) ;
+        faktoja = new String[] {"A", "BEN"};
 
-    public void faktaNappi(){
-        fakta.setText("faktaaa");
 
 
     }
+
+    public void faktaNappi(View v){
+        rand = (int) (Math.random() * 4);
+        fakta.setText(faktoja[rand]);
+
+
+    }
+
 
 }
